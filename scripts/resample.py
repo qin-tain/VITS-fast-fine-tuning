bf7cdc3d3f039ed64759493c45e6464e0b136fb0
qin-tain <20282043+qin-tain@users.noreply.github.com>
2023-09-14 05:58:51 +0800

    格式化scripts代码

--- scripts/resample.py
+++ scripts/resample.py
@@ -1,20 +1,33 @@
-import os
-import json
 import argparse
+import json
+import os
+
 import torchaudio
 
 
 def main():
-    with open("./configs/finetune_speaker.json", 'r', encoding='utf-8') as f:
+    with open("./configs/finetune_speaker.json", "r", encoding="utf-8") as f:
         hps = json.load(f)
-    target_sr = hps['data']['sampling_rate']
+    target_sr = hps["data"]["sampling_rate"]
     filelist = list(os.walk("./sampled_audio4ft"))[0][2]
     if target_sr != 22050:
         for wavfile in filelist:
-            wav, sr = torchaudio.load("./sampled_audio4ft" + "/" + wavfile, frame_offset=0, num_frames=-1,
-                                      normalize=True, channels_first=True)
+            wav_file_path = "./sampled_audio4ft" + "/" + wavfile
+            wav, sr = torchaudio.load(
+                wav_file_path,
+                frame_offset=0,
+                num_frames=-1,
+                normalize=True,
+                channels_first=True,
+            )
             wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=target_sr)(wav)
-            torchaudio.save("./sampled_audio4ft" + "/" + wavfile, wav, target_sr, channels_first=True)
+            torchaudio.save(
+                wav_file_path,
+                wav,
+                target_sr,
+                channels_first=True,
+            )
+
 
 if __name__ == "__main__":
     main()
